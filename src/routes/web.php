bc0ada8226a6f93b8bb54aa8bd57e33a7863c350
Nicolas Velez <nicolas.velez99@hotmail.com>
2022-12-29 17:28:21 -0500

    [feat]: added new routes to test

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,13 @@
 <?php 
+require_once __DIR__.'/../../vendor/autoload.php';
+
 $router = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
 
 
     // Class method endpoints
-    $r->addRoute("GET", "/dashboard", [UserController::class, "getUsers"]);
+    $r->addRoute('GET', '/hello', function() {
+        echo "Hola mundo";
+      });
+    $r->addRoute("GET", "/login", [AuthController::class, "index"]);
     $r->addRoute("POST", "/users/{id}", [UserController::class, "updateUser"]);
 });
